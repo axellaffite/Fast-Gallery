9adb4153d35d9e17f404266be712f0a3fd90bb57
Axel Laffite <laffite.axel81@gmail.com>
2020-09-25 09:22:13 +0200

    refactor: Target reset at each onBindView

--- FastGallery/src/main/java/com/axellaffite/fastgallery/ImageViewPager.kt
+++ FastGallery/src/main/java/com/axellaffite/fastgallery/ImageViewPager.kt
@@ -14,7 +14,7 @@ import com.squareup.picasso.Target
 class ImageViewPager<Image>(var images: List<Image>, private val converter: (Image) -> Uri) :
     RecyclerView.Adapter<ImageViewPager.ImageViewHolder>() {
 
-    class ImageViewHolder(val view: SubsamplingScaleImageView, val target: Target): RecyclerView.ViewHolder(view)
+    class ImageViewHolder(val view: SubsamplingScaleImageView, var target: Target?): RecyclerView.ViewHolder(view)
 
     var onImageLoadListener: OnImageLoadListener? = null
 
@@ -22,35 +22,37 @@ class ImageViewPager<Image>(var images: List<Image>, private val converter: (Ima
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.image_displayer_view_holder, parent, false) as SubsamplingScaleImageView
 
-        return ImageViewHolder(view, object: Target {
+        return ImageViewHolder(view, null)
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        holder.target = object: Target {
             override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
                 bitmap?.let {
                     val res = onImageLoadListener?.onSuccess(bitmap) ?: bitmap
-                    view.setImage(ImageSource.bitmap(res))
+                    holder.view.setImage(ImageSource.bitmap(res))
                 }
             }
 
             override fun onBitmapFailed(e: Exception?, errorDrawable: Drawable?) {
                 e?.printStackTrace(System.err)
                 onImageLoadListener?.onError()?.let {
-                    view.setImage(ImageSource.bitmap(it))
+                    holder.view.setImage(ImageSource.bitmap(it))
                 }
             }
 
             override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
                 onImageLoadListener?.onPrepare()?.let {
-                    view.setImage(ImageSource.bitmap(it))
+                    holder.view.setImage(ImageSource.bitmap(it))
                 }
             }
 
-        })
-    }
+        }
 
-    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
         Picasso
             .get()
             .load(converter(images[position]))
-            .into(holder.target)
+            .into(holder.target!!)
     }
 
     override fun getItemCount() = images.size
