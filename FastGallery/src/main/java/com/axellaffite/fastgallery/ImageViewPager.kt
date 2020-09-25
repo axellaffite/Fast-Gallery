532784d5e8c1c55c8b3416169a70d3bdef882fb9
Axel Laffite <laffite.axel81@gmail.com>
2020-09-25 08:23:10 +0200

    refactor: A reference of the Target is holden in the ImageViewHolder to avoid it to be garbage collected.

--- FastGallery/src/main/java/com/axellaffite/fastgallery/ImageViewPager.kt
+++ FastGallery/src/main/java/com/axellaffite/fastgallery/ImageViewPager.kt
@@ -4,55 +4,56 @@ import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.davemorrissey.labs.subscaleview.ImageSource
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView
 import com.squareup.picasso.Picasso
 import com.squareup.picasso.Target
-import java.lang.Exception
 
 class ImageViewPager<Image>(var images: List<Image>, private val converter: (Image) -> Uri) :
     RecyclerView.Adapter<ImageViewPager.ImageViewHolder>() {
 
-    class ImageViewHolder(val view: View): RecyclerView.ViewHolder(view)
+    class ImageViewHolder(val view: SubsamplingScaleImageView, val target: Target): RecyclerView.ViewHolder(view)
 
     var onImageLoadListener: OnImageLoadListener? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.image_displayer_view_holder, parent, false)
+            .inflate(R.layout.image_displayer_view_holder, parent, false) as SubsamplingScaleImageView
 
-        return ImageViewHolder(view)
+        return ImageViewHolder(view, object: Target {
+            override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
+                println("Loaded")
+                bitmap?.let {
+                    val res = onImageLoadListener?.onSuccess(bitmap) ?: bitmap
+                    view.setImage(ImageSource.bitmap(res))
+                }
+            }
+
+            override fun onBitmapFailed(e: Exception?, errorDrawable: Drawable?) {
+                println("failed")
+                e?.printStackTrace(System.err)
+                onImageLoadListener?.onError()?.let {
+                    view.setImage(ImageSource.bitmap(it))
+                }
+            }
+
+            override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
+                println("prepare")
+                onImageLoadListener?.onPrepare()?.let {
+                    view.setImage(ImageSource.bitmap(it))
+                }
+            }
+
+        })
     }
 
     override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
-        val container = holder.view
-        if (container is SubsamplingScaleImageView) {
-            Picasso.get().load(converter(images[position])).into(object: Target {
-                override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
-                    bitmap?.let {
-                        val res = onImageLoadListener?.onSuccess(bitmap) ?: bitmap
-                        container.setImage(ImageSource.bitmap(res))
-                    }
-                }
-
-                override fun onBitmapFailed(e: Exception?, errorDrawable: Drawable?) {
-                    e?.printStackTrace(System.err)
-                    onImageLoadListener?.onError()?.let {
-                        container.setImage(ImageSource.bitmap(it))
-                    }
-                }
-
-                override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
-                    onImageLoadListener?.onPrepare()?.let {
-                        container.setImage(ImageSource.bitmap(it))
-                    }
-                }
-
-            })
-        }
+        Picasso
+            .get()
+            .load(converter(images[position]))
+            .into(holder.target)
     }
 
     override fun getItemCount() = images.size
